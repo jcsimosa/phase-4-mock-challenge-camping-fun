96f79f17816d832cca161f4cbca4ef06d45a4066
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-09-07 12:31:44 -0500

    commit tree 04476a3d256a945572e878541b00b3da96be361e

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,31 @@
+class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+       
+    def index 
+        campers = Camper.all 
+        render json: campers,  status: :ok
+    end
+    
+    def show 
+        camper = Camper.find(params[:id])
+        render json:camper, serializer: CamperWithActivitySerializer
+    end
+    
+    def create
+        camper = Camper.create!(camper_params)
+        render json:camper, status: :created
+    end
+    
+    private
+    def render_not_found_response(error)
+        render json:{error: 'Camper not found'},status: :not_found
+    end
+    def render_unprocessable_entity(invalid)
+        render json:{errors: invalid.record.errors.full_messages},status: :unprocessable_entity
+    end
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+end
